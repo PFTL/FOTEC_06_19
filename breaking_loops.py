e134f187ee37d93bc64ffed645da0ebce3eeedd6
Aquiles Carattino <aquiles@aquicarattino.com>
2019-06-26 16:35:11 +0200

    Clean up after lunch break

--- breaking_loops.py
+++
@@ -1,6 +0,0 @@
-var = [i for i in range(1000)]
-
-for element in var:
-    if element == 100:
-        break
-    print(element)
--- test_is.py
+++
@@ -1,9 +0,0 @@
-a = [1]
-b = [1]
-c = a
-
-print(a is b)
-print(c is a)
-print(a == b)
-print(id(b))
-print(id(a))
